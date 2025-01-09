9f34def2e13756ad79ff01f90d3059c58e9df0de
Muhammad-Abdelsattar <muhammadabdalsattar@gmail.com>
2025-01-09 16:07:04 +0000

    Added basic model export logic.

--- arabic_tts/__main__.py
+++ arabic_tts/__main__.py
@@ -2,7 +2,8 @@ import os
 import typer
 import wandb
 from omegaconf import OmegaConf
-from .factory import create_trainer
+from .factory import create_trainer, create_model
+from .utils import get_tokenizer_config, save_tokenizer_config
 
 cli = typer.Typer()
 
@@ -22,9 +23,22 @@ def train(config: str = typer.Option("config.yaml", "-c", "--config", help="Conf
     
     
 @cli.command()
-def export_model(checkpoint_path: str = typer.Option("", "-c", "--config", help="Configuration path."),):
-    print("Exporting model...")
-    print("The export method isn't implemented yet.")
-    
+def export_model(config: str = typer.Option("config.yaml", "-c", "--config", help="Configuration path."),
+                 checkpoint_path: str = typer.Option("", "--checkpoint-path", help="Path to the checkpoint to be exported."),
+                 export_path: str = typer.Option("", "--export-path", help="Path to save the exported model and metadata.")):
+    config = OmegaConf.load(config)
+    if not os.path.exists(checkpoint_path):
+        raise FileNotFoundError(f"Checkpoint path {checkpoint_path} does not exist.")
+    if not os.path.exists(export_path):
+        os.makedirs(export_path)
+    config = OmegaConf.to_container(config, resolve=True)
+    model, config = create_model(config)
+    model.load_checkpoint(config=config, checkpoint_path=checkpoint_path)
+    print(f"> Model loaded from {checkpoint_path}.")
+    print(f"> Exporting model to {export_path}.")
+    model.export_onnx(export_path)
+    print(f"> Model exported to {export_path} successfully.")
+
+
 if __name__ == "__main__":
     cli()
